9ef0e9528612f7daa2b966e9cef1c7b06ef55f1e
GlaggleWeb <rhpjmf7t2s@privaterelay.appleid.com>
2026-02-27 13:10:33 +0100

    Create favoriten.js

---
+++ favoriten.js
@@ -0,0 +1,9 @@
+// favoriten.js
+const meineLieblingsSeiten = [
+    { title: "Glaggle", desc: "Glaggle Your life!", url: "https://glaggle.ch" },
+    { title: "YouTube", desc: "Schaue die neuesten Videos weltweit.", url: "https://youtube.com" },
+    { title: "Glaggle Zahlenraten", desc: "Ein lustiges Spiel, um Zahlen zu erraten.", url: "https://glaggle.ch/zahlenraten" },
+    { title: "Glame AI", desc: "Nutze den intelligenten Ki Chatbot von Glaggle", url: "https://glaggle.ch/glame" },
+    { title: "Glaggle Mail", desc: "Sichere E-Mails für dein Glaggle-Konto.", url: "https://mail.glaggle.ch" }
+    // Hier kannst du einfach weitere Zeilen hinzufügen
+];
